0febfe428252905a067fcf6a6fd6104c75cffd60
hitesh suthar <hiteshsuthar1520@gmail.com>
2024-06-30 14:18:13 +0530

    Added flight-create request middleware

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,21 +2,26 @@ const express = require("express");
 const CityController = require("../../controllers/city-controller");
 const FlightController = require("../../controllers/flight.controller");
 const AirportController = require('../../controllers/airport-controller');
+const { FlightMiddlewares } = require('../../middlewares/index')
 const router = express.Router();
 
 //city routes
-router.post('/city',CityController.create);
-router.post('/cities',CityController.createMulti);
-router.delete('/city/:id',CityController.destroy);
-router.get('/city/:id',CityController.get);
-router.get('/city',CityController.getAll);
-router.patch('/city/:id',CityController.update);
+router.post('/city', CityController.create);
+router.post('/cities', CityController.createMulti);
+router.delete('/city/:id', CityController.destroy);
+router.get('/city/:id', CityController.get);
+router.get('/city', CityController.getAll);
+router.patch('/city/:id', CityController.update);
 
 //flight routes
 
-router.post('/flights',FlightController.create);
-router.get('/flights',FlightController.getAll);
+router.post(
+    '/flights',
+    FlightMiddlewares.validateCreateFlight,
+    FlightController.create
+);
+router.get('/flights', FlightController.getAll);
 
-router.post('/airports',AirportController.create);
+router.post('/airports', AirportController.create);
 
 module.exports = router;
